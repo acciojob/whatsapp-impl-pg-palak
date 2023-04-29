696745c254a66df2da875efd33bda74c2a936bae
pg-palak <pgpalakgupta17gmail.com>
2023-04-29 20:27:56 +0530

    assignment done

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -1,5 +1,6 @@
 package com.driver;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Message {
@@ -7,4 +8,21 @@ public class Message {
     private String content;
     private Date timestamp;
 
+    public int getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public Message(int id, String content) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = new Date();
+    }
 }
